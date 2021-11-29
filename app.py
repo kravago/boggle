e12097c92bcd61eb660d677e037ebaf6b7ca393a
Engines-screaming <karl.ravago@gmail.com>
2021-11-28 21:46:28 -0800

    iadded home route

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from boggle import Boggle
+from flask import Flask, render_template
+
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('base.html')
