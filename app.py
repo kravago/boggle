fd10e1fe7204463f5af871139e5e0144b24f7fdd
Engines-screaming <karl.ravago@gmail.com>
2021-11-29 01:59:42 -0800

    added debug toolbar

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from boggle import Boggle
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
+from flask_debugtoolbar import DebugToolbarExtension
 
 # game setup
 boggle_game = Boggle()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'letsplayboggle'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 
 @app.route('/')
 def home():
     session['board'] = boggle_game.make_board()
     return render_template('board.html')
+
+@app.route('/submit-word')
+def submit_word():
+    '''Handle the submission of guesses'''
+    guess = request.args['guess']
