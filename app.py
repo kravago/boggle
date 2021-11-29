74b8fac3ef0611ff5ca6b35ca1f9e696c632f391
Engines-screaming <karl.ravago@gmail.com>
2021-11-28 22:12:03 -0800

    homepage shows board

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 
 
 boggle_game = Boggle()
+board = boggle_game.make_board()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('board.html', board=board)
